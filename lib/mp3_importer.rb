0957cbb015c4f00d89288867e94811f9380853d8
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-21 01:14:48 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -10,11 +10,12 @@ class MP3Importer
   end
   
   def files
-    
-
-    
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
-  
+
+  def import
+    files.each{|f| Song.new_by_filename(f)}
+  end
   
   
   
