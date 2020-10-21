f77b0e427edaf5bc08ee875a567fdff45e5a6a57
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-10-21 00:49:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,13 +23,21 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-      if self.all.find do |artist|
-        artist.name == name
+      if self.find(name)
+          self.find(name)
         end
-        self.name
+        
       else
         self.new(name)
       end
+    
+  end
+  
+  
+  def self.find(name)
+    self.all.find do |artist|
+      artist.name == name
+    end
     
   end
   
